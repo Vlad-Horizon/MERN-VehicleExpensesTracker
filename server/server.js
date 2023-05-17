8d722ba8769181d68839b40867e01f116fb42509
Vlad Horizon <vlad.horizon.02032003@gmail.com>
2023-05-17 22:51:14 +0300

    [server] - up limit requests data

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ forcedCreateFolder(`./${folderToSaveImg}`);
 // ----------------------------------------------------------------------
 
 const app = express();
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({}));
 app.use(cors());
